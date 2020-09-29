53c531ba9f8964d786089b18e2f3c23a323e4aa7
Teethew <thiagjack@gmail.com>
2020-09-29 16:03:02 -0300

    template do feed e barra lateral aula02

---
+++ blogPessoal/src/app/perfil-lateral/perfil-lateral.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-perfil-lateral',
+  templateUrl: './perfil-lateral.component.html',
+  styles: [
+  ]
+})
+export class PerfilLateralComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
